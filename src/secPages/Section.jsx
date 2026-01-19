9bb54a0863e7d56a80f4ecc817ef8576891e570d
Prerna Sharma <prernasharma0925@gmail.com>
2026-01-19 16:25:47 +0530

    sec

--- src/secPages/Section.jsx
+++ src/secPages/Section.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import SwiperImg from './SwiperImg'
+
 
 const Section = () => {
     const navigate = useNavigate()
-  return (
-    <div className=' flex gap-20 px-5 items-center justify-between h-screen'>
-      <div className='1/3 flex flex-col justify-center gap-10 '>
-        <h1 className='text-7xl font-semibold'>Soft Comfort <br /> Bold Looks</h1>
-      <button className='border-2 w-fit py-1 px-4 text-xl font-semibold rounded-full  ' onClick={()=>{
-        navigate('/products')
-      }}>Collection</button>
-      </div>
-      <div className='2/3 '>thrfd</div>
-    </div>
-  )
+    return (
+        <div className=' flex gap-10 px-5 w-[100%]  justify-between h-screen items-center'>
+
+
+            <div className='w-[30%] flex flex-col justify-center gap-10'>
+                <h1 className='text-6xl font-semibold uppercase'>Soft Comfort <br /> Bold Looks</h1>
+                <div className='flex flex-col gap-3'>
+                    <p className='text-xs text-violet-200'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis et nam laborum consectetur cupiditate debitis reprehenderit consequatur delectus doloribus ullam.</p>
+                    <button className='border-2  w-fit py-1 px-4 text-xl font-semibold rounded-full hover:bg-violet-700 ' onClick={() => {
+                        navigate('/products')
+                    }}>Collection</button>
+                </div>
+            </div>
+
+
+            <div className='w-[65%] flex gap-5 justify-between'>
+                <SwiperImg />
+            </div>
+
+
+        </div>
+    )
 }
 
 export default Section
---
+++ src/secPages/SwiperImg.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Autoplay, Navigation, Pagination } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+const SwiperImg = () => {
+  const Data = [
+    {
+      img: "https://i.pinimg.com/736x/c5/ce/b0/c5ceb0ce58e6469a3229a4293b9ff617.jpg",
+      title: "Fashion",
+      para: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia, molestias?",
+    },
+    {
+      img: "https://i.pinimg.com/736x/00/fd/29/00fd29f4dfbf1360d24d667163e7a936.jpg",
+      title: "Fashion",
+      para: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia, molestias?",
+    },
+    {
+      img: "https://i.pinimg.com/736x/4e/17/de/4e17de27db011d9b8958e401f21bf598.jpg",
+      title: "Fashion",
+      para: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia, molestias?",
+    },
+    {
+      img: "https://i.pinimg.com/736x/15/b7/6c/15b76cd086d88cb30f2ee8d0e0567308.jpg",
+      title: "Fashion",
+      para: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia, molestias?",
+    },
+    {
+      img: "https://i.pinimg.com/736x/22/6e/2a/226e2a3c262db262eef6e2373a61100c.jpg",
+      title: "Fashion",
+      para: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia, molestias?",
+    },
+    {
+      img: "https://i.pinimg.com/736x/1f/23/3a/1f233a64611fce0cde8f7db56d2a7ed5.jpg",
+      title: "Fashion",
+      para: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia, molestias?",
+    },
+  ]
+  return (
+    <div className='p-5 mt-14 w-full'>
+      <Swiper
+        modules={[Autoplay, Pagination, Navigation]}
+        autoplay={{ delay: 2000, disableOnInteraction: false }}
+        spaceBetween={20}
+        slidesPerView="auto"
+        loop={true}
+        navigation={false}
+        pagination={{ clickable: true }}
+      >
+        {Data.map((elem, idx) => {
+          return (
+            <SwiperSlide key={idx} className='!w-auto'>
+              <div className='relative w-[250px] mx-auto h-[450px] rounded-2xl overflow-hidden group'>
+                <img className='object-center object-cover h-full w-full' src={elem.img} alt="" />
+
+                <div className='absolute p-5 z-10 left-0 bottom-0 bg-[#ffffff2e] backdrop-blur-sm opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
+                  <h1 className='text-xl font-semibold  text-violet-900'>{elem.title}</h1>
+                  <p className='text-xs text-violet-400'>{elem.para}</p>
+                </div>
+              </div>
+            </SwiperSlide>
+          )
+        })}
+      </Swiper>
+    </div>
+  )
+}
+
+export default SwiperImg
